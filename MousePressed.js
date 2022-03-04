a45b2c26e526c18032888e0925fc3bcf9ece667c
ligaarc000 <100947929+ligaarc000@users.noreply.github.com>
2022-03-04 14:41:54 -0600

    Create MousePressed.js

---
+++ MousePressed.js
@@ -0,0 +1,35 @@
+function mousePressed(){
+  if (Slide==0){
+  
+}
+if (Slide==1){
+  
+}
+  if (Slide==2){
+  
+}
+  if (Slide==3){
+  
+}
+  if (Slide==4){
+  
+}
+  if (Slide==5){
+  
+}
+  if (Slide==6){
+  
+}
+  if (Slide==7){
+  
+}
+  if (Slide==8){
+  
+}
+  if (Slide==9){
+  
+}
+  if (Slide==10){
+  
+}
+}
