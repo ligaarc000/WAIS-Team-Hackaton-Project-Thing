43b038fe58602f6976037b8663961c3a8dc669a8
zapatmic000 <100948536+zapatmic000@users.noreply.github.com>
2022-03-04 15:08:03 -0600

    Update MousePressed.js

--- MousePressed.js
+++ MousePressed.js
@@ -19,9 +19,22 @@ if (Slide==1){
 //1921 July 1 Rise of chinese communists:
 //1922: British Empire Shrinks
 //1923: Great Kanto Earthquake
-  
+//1924: From Lenin to Stalin
+//1925: Scopes Monkey Trial
+//1927: Lindbergh Nonstop to Paris
+//1928: Earhart Crosses Atlantic
+//1929: Wall Street Crashes
+//1930: Ho Chi Minh Rises in Vietnam
+
 }
+
   if (Slide==3){
+//1930: Ho Chi Minh Rises in Vietnam
+//1931: Empire State Building Completed 
+//1933: FDR Elected
+//1933 Hitler Elected Chancellor
+//1934: Hitler Consolidates Power
+
 }
   if (Slide==4){
   
