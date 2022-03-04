c52c5e73365d111e29e1315515286f3b1d2f3206
zapatmic000 <100948536+zapatmic000@users.noreply.github.com>
2022-03-04 09:26:49 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,32 @@
 var Slide;
+var img0;
+var img1;
+var img2;
+var img3;
+var img4
+var img5
+var img6;
+var img7;
+var img8;
+var img9;
+var img10;
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 500);
   Time_slider = createSlider(0,10,0)
   Time_slider.style("width","400px")
   Time_slider.input(Update)
   Slide = Time_slider.value()
+  img0 = loadImage("map0.jpg")
+  img1 = loadImage("map1.jpg")
+  img2 = loadImage("map2.jpg")
+  img3 = loadImage("map3.jpg")
+  img4 = loadImage("map4.jpg")
+  img5 = loadImage("map5.jpg")
+  img6 = loadImage("map6.jpg")
+  img7 = loadImage("map7.jpg")
+  img8 = loadImage("map8.jpg")
+  img9 = loadImage("map9.jpg")
+  img10 = loadImage("map10.jpg")
   noLoop();
 }
 function Update() {
@@ -17,34 +39,38 @@ function draw() {
 }
 
 function Map() {
+  // first 5 archer maps
+if (Slide==0){
+  image(img0,0,0,width,height)
+}
 if (Slide==1){
-  ellipse(50,50,100,100)
+  image(img1,0,0,width,height)
 }
   if (Slide==2){
-
+  image(img2,0,0,width,height)
 }
   if (Slide==3){
-  ellipse(50,50,150,100)
+  image(img3,0,0,width,height)
 }
   if (Slide==4){
-  ellipse(50,50,100,150)
+  image(img4,0,0,width,height)
 }
   if (Slide==5){
-  ellipse(50,50,100,150)
+  image(img5,0,0,width,height))
 }
   if (Slide==6){
-  ellipse(50,50,100,150)
+  image(img6,0,0,width,height)
 }
   if (Slide==7){
-  ellipse(50,50,100,250)
+  image(img7,0,0,width,height)
 }
   if (Slide==8){
-  ellipse(80,50,100,150)
+  image(img8,0,0,width,height)
 }
   if (Slide==9){
-  ellipse(100,50,100,150)
+  image(img9,0,0,width,height)
 }
   if (Slide==10){
-  ellipse(150,50,100,150)
+  image(img10,0,0,width,height)
 }
 }
