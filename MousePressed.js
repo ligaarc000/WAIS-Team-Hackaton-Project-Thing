280ea747f7ec4cc73a44f240f7de274304754a51
ligaarc000 <100947929+ligaarc000@users.noreply.github.com>
2022-03-04 14:50:30 -0600

    Update MousePressed.js

--- MousePressed.js
+++ MousePressed.js
@@ -1,15 +1,27 @@
 function mousePressed(){
+console.log('test')
   if (Slide==0){
-  
+  //Russo-Japanese War (1904):
+
 }
 if (Slide==1){
-  
+  //Fall of the Qing dynasty(1911):
+  //First balkan War (1912): Second balkan war (1913):
+  //Assasination of Archduke Franz Ferdinand(1914):
+  //World War One(1914): 
+  //The Russian revolution(1917):The Russian civil war(1917):
+  //Treaty of versaille: (1919)
+  //Treaty of san germaine: (1919)
 }
   if (Slide==2){
+//Treaty of Sevres: (1920)
+//1920 Woman Suffrage: August 26 in Washington DC
+//1921 July 1 Rise of chinese communists:
+//1922: British Empire Shrinks
+//1923: Great Kanto Earthquake
   
 }
   if (Slide==3){
-  
 }
   if (Slide==4){
   
